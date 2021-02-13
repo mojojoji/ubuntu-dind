7f684376e77ba3d9dc1b90ecd947aca7f5cb8042
Joji Augustine <jojimail@gmail.com>
2021-02-13 23:14:21 +0530

    sysbox runc

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 docker \
     run \
-    --privileged \
+    --runtime=sysbox-runc \
     -it \
     -p 8080:8080 \
-    -v $(pwd)/.data/dockerlib:/var/lib/docker \
-    -v $(pwd)/.data/checkpoints:/checkpoints \
     -v $(pwd)/runProject.sh:/runProject.sh \
     dind
