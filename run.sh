385da15e7a2b3267abc41f4b3277a5c7980b9a3f
Joji Augustine <jojimail@gmail.com>
2021-02-13 20:59:10 +0530

    add .data folder

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker \
     --privileged \
     -it \
     -p 8080:8080 \
-    -v $(pwd)/dockerlib:/var/lib/docker \
-    -v $(pwd)/checkpoints:/checkpoints \
+    -v $(pwd)/.data/dockerlib:/var/lib/docker \
+    -v $(pwd)/.data/checkpoints:/checkpoints \
     -v $(pwd)/runProject.sh:/runProject.sh \
     dind
